bc4ab5475d8ce07feab9bbf87de61754a06526c1
kkm <kubasurdej@gmail.com>
2019-09-29 00:44:42 +0200

    Install jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    "roots": [
+        "<rootDir>/test/"
+    ],
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+  }
