aef8faf523c36d5ca344dacab4a72ae977a9e5d2
kkm <kubasurdej@gmail.com>
2019-09-29 17:04:39 +0200

    Create example test

--- test/helpers/jsonHelper.ts
+++ test/helpers/jsonHelper.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs'
 import { join } from 'path'
 
 const filePath = (path: string): string => {
-    return join(__dirname, path)
+    return join(__dirname, '..', path)
 }
 
 export const readDataFile = (path: string): string => {
