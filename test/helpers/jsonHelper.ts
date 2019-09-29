06dd1640d8c28f5945146b0d9eb72da101a87598
kkm <kubasurdej@gmail.com>
2019-09-29 16:53:28 +0200

    Use path instead of filename in jsonHelper

--- test/helpers/jsonHelper.ts
+++ test/helpers/jsonHelper.ts
@@ -1,16 +1,16 @@
 import { readFileSync } from 'fs'
 import { join } from 'path'
 
-const filePath = (filename: string): string => {
-    return join(__dirname, 'data', filename)
+const filePath = (path: string): string => {
+    return join(__dirname, path)
 }
 
-export const readDataFile = (filename: string): string => {
-    return readFileSync(filePath(filename), 'utf-8')
+export const readDataFile = (path: string): string => {
+    return readFileSync(filePath(path), 'utf-8')
 }
 
-export const getObjectFromFile = (filename: string): object => {
-    const stringifiedObject = readDataFile(filename)
+export const getObjectFromFile = (path: string): object => {
+    const stringifiedObject = readDataFile(path)
     
     return JSON.parse(stringifiedObject)
 }
