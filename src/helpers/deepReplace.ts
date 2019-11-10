5f786cd156f07f582939419ead3c9428d679d14d
kkm <kubasurdej@gmail.com>
2019-11-10 21:17:36 +0100

    Create deepReplace function

---
+++ src/helpers/deepReplace.ts
@@ -0,0 +1,13 @@
+const deepReplace = (obj: any, searchedKey: string, replacementString: string) => {
+    if (typeof(obj) === "object") {
+        for (const [key, value] of Object.entries(obj)) {
+            if (typeof(value) === "object") {
+                deepReplace(obj[key], searchedKey, replacementString)
+            } else if (typeof(value) === "string" && key == searchedKey) {
+                obj[key] = replacementString
+            }
+        }
+    }
+}
+
+export default deepReplace
