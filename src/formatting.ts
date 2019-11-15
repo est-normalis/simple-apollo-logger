ea4313889ad262dfd86e61344ebac35affc42a2d
kkm <kubasurdej@gmail.com>
2019-11-16 00:03:43 +0100

    Add advanced request variable filtering

--- src/formatting.ts
+++ src/formatting.ts
@@ -1,16 +1,21 @@
 import deepReplace from './helpers/deepReplace'
 
-const filterPasswordFromVariables = (variables: { password?: string }) => {
-    deepReplace(variables, "password", "[FILTERED]")
+const filterPasswordFromVariables = (variables: any, keyword: string, replacementText: string) => {
+    // todo: make this function pure
+    deepReplace(variables, keyword, replacementText)
 
     return variables
 }
 
-export const stringifiedRequestAttributes = ({ headers, variables, queryString, operationName }: request): string => {
-    const filteredVariables = filterPasswordFromVariables(variables)
+export const stringifiedRequestAttributes = ({ headers, variables, queryString, operationName }: request, variableFilter: VariableFilter | undefined): string => {
+    if (variableFilter) {
+        variableFilter.keywords.forEach((keyword) => {
+            filterPasswordFromVariables(variables, keyword, variableFilter.replacementText)
+        })
+    }
 
     const stringifiedHeaders = JSON.stringify(headers)
-    const stringifiedVariables = JSON.stringify(filteredVariables)
+    const stringifiedVariables = JSON.stringify(variables)
     const stringifiedQueryString = JSON.stringify(queryString).replace(/\s/g, '').replace(/\\n/g, " ")
 
     return `${operationName}\nHeaders: ${stringifiedHeaders}\nQueryString: ${stringifiedQueryString}\nVariables: ${stringifiedVariables}`
@@ -21,4 +26,9 @@ export interface request {
     queryString: any
     operationName: string
     variables: object
+}
+
+export interface VariableFilter {
+    keywords: [string]
+    replacementText: string
 }
