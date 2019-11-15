d4a882e866bdeca5df9f5e022109d5ad3f9a58bd
kkm <kubasurdej@gmail.com>
2019-11-15 23:30:53 +0100

    Create log extension options

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
 import { stringifiedRequestAttributes } from './formatting'
 
 export default class ApolloLogExtension {
+    private options: Options
+    private logger: Logger
+
+    constructor(options: Options) {
+        this.options = options
+        this.logger = options.logger
+    }
+
     requestDidStart(request: any): void {
-        console.log(stringifiedRequestAttributes(request))
+        this.logger.log(stringifiedRequestAttributes(request))
     }
   
     willSendResponse(object: any): void {
-      //console.log(JSON.stringify(object.graphqlResponse.data))
+      this.logger.log(JSON.stringify(object.graphqlResponse.data))
     }
 
     parsingDidStart(r: any): void {}
     validationDidStart(): void {}
     executionDidStart(r: any): void {}
+}
+
+export interface Options {
+    logger: Logger
+    logRequests: boolean
+    logResponses: boolean
+}
+
+export interface Logger {
+    log(msg: string): any
 }
