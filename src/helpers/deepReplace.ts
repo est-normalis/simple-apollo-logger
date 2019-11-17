13ec857a98dccc36ac19a7ebb0b51edf91f18778
kkm <kubasurdej@gmail.com>
2019-11-17 13:43:51 +0100

    Lint and prettify files, update configs
    Lint and prettify both manually and automagically,
    edit its config, copied from other project, to better match
    this library.

--- src/helpers/deepReplace.ts
+++ src/helpers/deepReplace.ts
@@ -1,13 +1,17 @@
-const deepReplace = (obj: any, searchedKey: string, replacementString: string) => {
-    if (typeof(obj) === "object") {
-        for (const [key, value] of Object.entries(obj)) {
-            if (typeof(value) === "object") {
-                deepReplace(obj[key], searchedKey, replacementString)
-            } else if (typeof(value) === "string" && key == searchedKey) {
-                obj[key] = replacementString
-            }
-        }
+const deepReplace = (
+  obj: any,
+  searchedKey: string,
+  replacementString: string
+) => {
+  if (typeof obj === 'object') {
+    for (const [key, value] of Object.entries(obj)) {
+      if (typeof value === 'object') {
+        deepReplace(obj[key], searchedKey, replacementString)
+      } else if (typeof value === 'string' && key === searchedKey) {
+        obj[key] = replacementString
+      }
     }
+  }
 }
 
 export default deepReplace
