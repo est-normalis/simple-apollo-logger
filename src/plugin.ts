7d429a58fef5ae0987e07852a65830d0b7a3e025
Adam <satko_a@op.pl>
2020-09-09 16:01:07 +0200

    Add plugin.ts

---
+++ src/plugin.ts
@@ -0,0 +1,4 @@
+import { UserOptions } from './options'
+import { ApolloServerPlugin } from 'apollo-server-plugin-base'
+const apolloLogPlugin = (opts: UserOptions = {}): ApolloServerPlugin => {
+};
