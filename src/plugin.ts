a0327013d05806d338c789c5946a5ab8ffb1842a
Adam <satko_a@op.pl>
2020-09-09 17:54:33 +0200

    Add function to plugin

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,4 +1,22 @@
-import { UserOptions } from './options'
+import { UserOptions, defaultOptions, Options } from './options'
+import { stringifiedRequestAttributes } from './formatting'
 import { ApolloServerPlugin } from 'apollo-server-plugin-base'
-const apolloLogPlugin = (opts: UserOptions = {}): ApolloServerPlugin => {
+
+
+export const apolloLogPlugin = (ops: UserOptions = {}): ApolloServerPlugin => {
+    const options: Options = Object.assign(defaultOptions, ops);
+    const log = (msg: string): void => {
+        options.logger(`${options.prefix()}${msg}`)
+    }
+    return {
+        requestDidStart(r) {
+            const isInspectionQuery = r.operationName === 'IntrospectionQuery'
+            const isIgnoredRequest =
+                options.ignoreSchemaRequest && isInspectionQuery
+            const shouldBeLogged = options.logRequests && !isIgnoredRequest
+            if (shouldBeLogged) {
+                log(stringifiedRequestAttributes(r, options.variableFilter))
+            }
+        }
+    }
 };
