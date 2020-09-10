47b9ee79d0ddc845b2d97d7eab80d85fc838ce0e
Adam <satko_a@op.pl>
2020-09-10 14:26:17 +0200

    Fix issue #23

--- src/formatting.ts
+++ src/formatting.ts
@@ -16,7 +16,7 @@ export const stringifiedRequestAttributes = (
   { variables, queryString, operationName }: ApolloRequest,
   variableFilter: VariableFilter | undefined | false
 ): string => {
-  const copyOfVariables = JSON.parse(JSON.stringify(variables))
+  const copyOfVariables = JSON.parse(variables === undefined ? String(variables) : JSON.stringify(variables))
   if (variableFilter) {
     variableFilter.keywords.forEach(keyword => {
       filterPasswordFromVariables(
