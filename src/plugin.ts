83d1c5c25aea36e11d5bc3f447a4b50daa9b51f7
kkm <kubasurdej@gmail.com>
2020-10-01 12:49:00 +0200

    Lint

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,15 +3,14 @@ import { stringifiedRequestAttributes } from './formatting'
 import { ApolloServerPlugin } from 'apollo-server-plugin-base'
 
 export default (ops: UserOptions = {}): ApolloServerPlugin => {
-  const options: Options = Object.assign(defaultOptions, ops);
+  const options: Options = Object.assign(defaultOptions, ops)
   const log = (msg: string): void => {
     options.logger(`${options.prefix()}${msg}`)
   }
   return {
     requestDidStart(r) {
       const isInspectionQuery = r.request.operationName === 'IntrospectionQuery'
-      const isIgnoredRequest =
-        options.ignoreSchemaRequest && isInspectionQuery
+      const isIgnoredRequest = options.ignoreSchemaRequest && isInspectionQuery
       const shouldBeLogged = options.logRequests && !isIgnoredRequest
       if (shouldBeLogged) {
         log(stringifiedRequestAttributes(r.request, options.variableFilter))
@@ -25,4 +24,4 @@ export default (ops: UserOptions = {}): ApolloServerPlugin => {
       }
     }
   }
-};
+}
